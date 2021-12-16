663832f6103035693eb855d635f60b3f73e6b7d9
tim <tmsteem82@gmail.com>
2021-12-16 17:09:11 +0100

    implement downloading HTML from URL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::StatusCode;
+
+mod utils;
+
+#[tokio::main]
+async fn main() {
+    let client = utils::get_client();
+    let url = "https://finance.yahoo.com";
+    let result = client.get(url).send().await.unwrap();
+
+    let raw_html = match result.status() {
+        StatusCode::OK => result.text().await.unwrap(),
+        _ => panic!("Something went wrong"),
+    };
+
+    println!("HTML: {}", raw_html);
 }
