40976215f7bca84475a25ee87d9b6b76e383b7e9
tim <tmsteem82@gmail.com>
2021-12-17 06:51:19 +0100

    implement header selectors for better data extraction

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,36 @@ async fn main() {
 
     let document = Html::parse_document(&raw_html);
     let article_selector = Selector::parse("a.js-content-viewer").unwrap();
+    let h2select = Selector::parse("h2").unwrap();
+    let h3select = Selector::parse("h3").unwrap();
 
     for element in document.select(&article_selector) {
         let inner = element.inner_html().to_string();
+        let mut h2el = element.select(&h2select);
+        let mut h3el = element.select(&h3select);
+
         let href = match element.value().attr("href") {
             Some(target_url) => target_url,
             _ => "no url found",
         };
+
+        match h2el.next() {
+            Some(elref) => {
+                println!("H2: {}", &elref.inner_html().to_string());
+                println!("Link: {}", &href);
+                continue;
+            }
+            _ => {}
+        }
+
+        match h3el.next() {
+            Some(elref) => {
+                println!("H3: {}", &elref.inner_html().to_string());
+                println!("Link: {}", &href);
+                continue;
+            }
+            _ => {}
+        }
 
         println!("Title: {}", &inner);
         println!("Link: {}", &href);
